c8198ddd80a39b6b8255fd54cca00689b6a86415
MMendes24 <mmbusiness1@protonmail.com>
2021-01-11 12:24:12 -0600

    Finished hangman game

--- hangman.py
+++ hangman.py
@@ -17,23 +17,27 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
-    # getting user input
-    while len(word_letters) > 0:
-        # letters used
-        print("You have used these letters: ", ' '.join(used_letters))
-        print(word)
+    lives = 6
 
+    # getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        print("You have", lives, "lives left and you have used these letters: ", ' '.join(used_letters))
+    
     # what current word is (ie W - R D)
         word_list = [
             letter if letter in used_letters else "-" for letter in word]
         print("Current word: "," ".join(word_list))
-        print(word_list)
 
         user_letter = input("Guess a letter: ").upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            
+            else:
+                lives -= 1
+                print("Letter is not in word")
         
         elif user_letter in used_letters:
             print("You have already guessed that letter")
@@ -42,6 +46,10 @@ def hangman():
             print("Please enter a letter, chief ")
 
     # gets here when len(word_letters) == 0
+    if lives == 0:
+        print("You died, sorry. The word was", word)
+    else:
+        print("You guessed the word!", word,)
 
 print(hangman())
 
